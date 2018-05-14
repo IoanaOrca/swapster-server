593a8410fec556e7eac211159a2cc3d66ea08d55
Ioana Iordache <ioanaorka@gmail.com>
2018-05-14 10:17:56 +0200

    pages

--- routes/items.js
+++ routes/items.js
@@ -5,29 +5,32 @@ const mongoose = require('mongoose');
 
 const Item = require('../models/item')
 
-// router.get('/', (req, res, next) => {
-//   const criteria = {};
-//   if (req.query.terms) {
-//     criteria['$regexp'] = terms
-//   }
-//   Item.find (criteria)
-//   .then((result) => {
-//     res.json(result)
-//   })
-//   .catch(next)
-// });
+router.get('/', (req, res, next) => {
+  const criteria = {};
+  if (req.query.terms) {
+    criteria['$regexp'] = req.query.terms;
+    criteria['$options'] = 'i';
+    console.log(criteria);
+  }
+  Item.find({title:criteria})
+  .then((result) => {
+    res.json(result)
+  })
+  .catch(next)
+});
 
 router.post('/', (req, res, next) => {
   const title = req.body.title;
   const price = Number(req.body.price);
   const description = req.body.description;
   const owner = req.session.currentUser._id;
+  const sold = false;
 
   if (!title || !price || !description){
     return res.status(422).json({code: 'unprocessable-entity'});
   }
 
-  const newItem = new Item({title, price, description, owner});
+  const newItem = new Item({title, price, description, owner,sold});
 
   newItem.save()
   .then((result) => {
@@ -47,42 +50,42 @@ router.get('/:id', (req, res, next) => {
   .catch(next)
 });
 
-// router.put('/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
 
-// // if not mongoose type
-// if (!mongoose.Types.ObjectId.isValid(req.params.id))  {
-//   return res.status(422).json({code : 'unprocessable-entity'})
-// }
+// if not mongoose type
+if (!mongoose.Types.ObjectId.isValid(req.params.id))  {
+  return res.status(422).json({code : 'unprocessable-entity'})
+}
 
-//   const newData = {
-//     title: req.body.title,
-//     posterUrl: req.body.posterUrl,
-//     price: req.body.price
-//   }
+  const newData = {
+    title: req.body.title,
+    price: req.body.price,
+    description: req.body.description
+  }
 
-//   //update the new one from mogoose --without user experience
-//   const options ={
-//     new: true
-//   }
-//   //if there is no movie with this id
-//   Movie.findById(req.params.id)
-//   .then((result) => {
-//     if(!result) {
-//       return res.status(404).json({code: 'not-found'});
-//     }
+  //update the new one from mogoose --without user experience
+  const options ={
+    new: true
+  }
+  //if there is no movie with this id
+  Item.findById(req.params.id)
+  .then((result) => {
+    if(!result) {
+      return res.status(404).json({code: 'not-found'});
+    }
     
-//     result.title=newData.title;
-//     result.year=newData.year;
-//     result.posterUrl=newData.posterUrl;
+    result.title=newData.title;
+    result.price=newData.price;
+    result.description=newData.description;
 
-//     result.save()
-//     .then(()=> {
-//       res.json(result);
-//     })
-//     .catch(next);
-//   })
-//   .catch(next)
-// });
+    result.save()
+    .then(()=> {
+      res.json(result);
+    })
+    .catch(next);
+  })
+  .catch(next)
+});
 
 
 router.delete('/:id', (req, res, next) =>{
